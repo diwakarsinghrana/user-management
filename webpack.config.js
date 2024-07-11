622fec8765775bc030b5be84a69181a6b4619d8f
diwakarsinghrana <diwakarsinghrana3@gmail.com>
2024-07-12 01:24:46 +0530

    testing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,28 @@ const path = require('path');
 module.exports = {
     entry: './src/main.ts',
     target: 'node',
+    mode: 'development', // or 'development'
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    externals: {
+        // Exclude node_modules from being bundled
+        'cache-manager': 'commonjs cache-manager',
+        'cache-manager-redis-store': 'commonjs cache-manager-redis-store',
+        '@nestjs/websockets': 'commonjs @nestjs/websockets',
+        '@nestjs/microservices': 'commonjs @nestjs/microservices',
     },
 };
